05429702c87e6d1e620e6d1fb3a670d7d4c6f1dc
iliassanati <iliassanati@gmail.com>
2024-02-10 11:46:40 +0100

    feat/ adding form to add new snippet"

---
+++ src/actions/index.ts
@@ -0,0 +1,63 @@
+'use server';
+
+import { db } from '@/db';
+import { redirect } from 'next/navigation';
+
+export async function editSnippet(id: number, code: string) {
+  await db.snippet.update({
+    where: { id },
+    data: { code },
+  });
+
+  redirect(`/snippets/${id}`);
+}
+
+export async function deleteSnippet(id: number) {
+  await db.snippet.delete({
+    where: { id },
+  });
+
+  redirect('/');
+}
+
+export async function createSnippet(
+  formaState: { message: string },
+  formData: FormData
+) {
+  // Check the user's inputs and make sure they're valid
+  const title = formData.get('title');
+  const code = formData.get('code');
+
+  if (typeof title !== 'string' || title.length < 3) {
+    return {
+      message: 'Title must be longer',
+    };
+  }
+
+  if (typeof code !== 'string' || code.length < 10) {
+    return {
+      message: 'Code must be longer',
+    };
+  }
+
+  try {
+    // Create a new record un the database
+    await db.snippet.create({
+      data: {
+        title,
+        code,
+      },
+    });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      return {
+        message: error.message,
+      };
+    } else {
+      return { message: 'Something went wrong' };
+    }
+  }
+
+  // Redirect tje user back to the root route
+  redirect('/');
+}
