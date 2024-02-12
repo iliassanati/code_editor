0252960815a7f1484d687952ccb8943f0e7ad8f6
iliassanati <iliassanati@gmail.com>
2024-02-12 10:15:07 +0100

    feat/ adding caching

--- src/app/snippets/[id]/not-found.tsx
+++ src/app/snippets/[id]/not-found.tsx
@@ -1,7 +1,7 @@
 export default function SnippetNotFound() {
   return (
     <div>
-      <h1 className='text-xl bold'>Sorry, but we couldn't find that page</h1>
+      <h1 className='text-xl bold'>Sorry, but we couldnt find that page</h1>
     </div>
   );
 }
